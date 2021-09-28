5079681accbe2e4ed5e0b26d006e730976ff59df
Nickc123z <90527489+Nickc123z@users.noreply.github.com>
2021-09-27 23:08:47 -0400

    Add files via upload

---
+++ E24Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class E24Test {
+
+    @Test
+    void anagram(int s1,int s2) {
+
+        assertEquals(s1,s2);
+
+
+    }
+}
