a97c81c6b038c9dba360e785aa11280dc7981175
Nickc123z <90527489+Nickc123z@users.noreply.github.com>
2021-09-27 23:08:52 -0400

    Add files via upload

---
+++ E25Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class E25Test {
+
+    @Test
+    public static void main(String[] args){
+
+        String st;
+
+        assertEquals(8, st.size());
+
+
+
+    }
+}
