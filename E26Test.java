33edc72e466ca0c1628f5d6c13fe20ea3a08e397
Nickc123z <90527489+Nickc123z@users.noreply.github.com>
2021-09-27 23:08:59 -0400

    Add files via upload

---
+++ E26Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class E26Test {
+
+    @Test
+    public static void main(String[] args) {
+
+        int high = 100;
+        int low = 1;
+        assertTrue("Error, apr is too high", high < apr);
+        assertTrue("Error, apr is too low",  low  > apr);
+
+    }
+}
